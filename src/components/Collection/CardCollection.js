92230189f21c7f7a87e569f14ae2a63e6331e70d
mohammad <mohammadmajidi27@gmail.com>
2022-05-30 10:39:40 +0430

    styled collection section

--- src/components/Collection/CardCollection.js
+++ src/components/Collection/CardCollection.js
@@ -6,8 +6,8 @@ import SingleCard from "./SingleCard";
 // row row-cols-sm-2  row-cols-md-3 row-cols-lg-4 row-cols-xl-5 gy-5
 const CardCollection = () => {
     return (
-        <Container>
-            <h2 className=" text-center heading"><span>A collection of best</span></h2>
+        <Container className='collectionSection mt-5'>
+            <h2 className="text-center fw-bold"><span>A collection of best</span></h2>
             <Row className='justify-content-center mt-5'>
                 {myCollectionFirstRow.map(el => <Col><SingleCard key={el.id} name={el.name} image={el.image}/></Col>)}
             </Row>
--- src/components/Collection/SingleCard.js
+++ src/components/Collection/SingleCard.js
@@ -5,13 +5,17 @@ import {Card} from "react-bootstrap";
 
 const SingleCard = (props) => {
     return (
-        <Card className='border-0 fw-bold w-100 h-100 shadow singleCard'>
-            <Card.Img className={'singleCard__cardImage m-auto'} variant="top" src={props.image}
-                      style={{width: props.name === 'clinque' ? '50px' : '80px'}}/>
-            <Card.Body>
-                <Card.Title className={'text-center singleCard__brandCard'}>{props.name}</Card.Title>
-            </Card.Body>
-        </Card>
+        <div className='cardContainer'>
+            <Card
+                className={props.name === 'elf' ? 'border-0 fw-bold w-100 h-100 shadow cardContainer__singleCard__elf' : 'border-0 fw-bold w-100 h-100 shadow cardContainer__singleCard'}>
+                <div className='cardContainer__singleCard__imageContainer'>
+                    <Card.Img className={'singleCard__imageContainer__imageCard'} variant="top" src={props.image}/>
+                </div>
+                <Card.Body>
+                    <Card.Title className={'text-center cardContainer__singleCard__brandCard'}>{props.name}</Card.Title>
+                </Card.Body>
+            </Card>
+        </div>
 
     );
 };
