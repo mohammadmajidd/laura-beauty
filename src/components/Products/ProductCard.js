85a9567f7713e6e5beaeec09b1ce2bc906f717eb
mohammad <mohammadmajidi27@gmail.com>
2022-05-30 12:43:34 +0430

    styled product section

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Card} from "react-bootstrap";
 import './ProductCard.scss'
+
 const ProductCard = ({product}) => {
     return (
-        <Card  style={{ width: '18rem' }} className={'productCard border-0 shadow p-3 rounded-3'}>
-            <div className={'d-flex justify-content-between productCard__cardHeader'}>
-                <div><img alt='icon' src={product.icon}/></div>
+        <Card style={{width: '18rem'}} className={'productCard border-0 shadow p-3 rounded-3'}>
+            <div className={'d-flex justify-content-between align-items-center productCard__cardHeader'}>
+                <img alt='icon' src={product.icon}/>
                 <div>new</div>
             </div>
             <div className={'productCard__imageContainer'}>
-                <Card.Img variant="top" src={product.image} />
+                <Card.Img variant="top" src={product.image}/>
             </div>
 
             <Card.Body>
