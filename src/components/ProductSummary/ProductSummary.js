7f6e93b6a60dfaf5967f156026362b7cef71aebe
mohammad <mohammadmajidi27@gmail.com>
2022-05-30 11:11:42 +0430

    styled ProductSummary section

--- src/components/ProductSummary/ProductSummary.js
+++ src/components/ProductSummary/ProductSummary.js
@@ -3,29 +3,31 @@ import {Col, Container, Row} from "react-bootstrap";
 import brush from '../../assets/productSummary/left.png'
 import palette from '../../assets/productSummary/img r.png'
 import './ProductSummary.scss'
+
 const ProductSummary = () => {
     return (
-        <div>
-            <Container className={'productSummary'}>
-                <h2 className={'text-center'}>Various products</h2>
-                <Row>
-                    <Col><img src={brush}/></Col>
-                    <Col className={'d-flex justify-content-center flex-column align-items-center productSummary__category'}>
-                        <p>6</p>
-                        <p>category</p>
-                    </Col>
-                    <Col className={'d-flex justify-content-center flex-column align-items-center productSummary__brands'}>
-                        <p>10</p>
-                        <p>brands</p>
-                    </Col>
-                    <Col className={'d-flex justify-content-center flex-column align-items-center productSummary__products'}>
-                        <p>9<span>k</span></p>
-                        <p>products</p>
-                    </Col>
-                    <Col><img src={palette}/></Col>
-                </Row>
-            </Container>
-        </div>
+        <Container>
+            <h2 className=" text-center fw-bold mt-5"><span>Various products</span></h2>
+
+            <Row className={'productSummary'}>
+                <Col><img alt={brush} src={brush}/></Col>
+                <Col
+                    className={'d-flex justify-content-center flex-column align-items-center productSummary__category'}>
+                    <p>6</p>
+                    <p>category</p>
+                </Col>
+                <Col className={'d-flex justify-content-center flex-column align-items-center productSummary'}>
+                    <p>10</p>
+                    <p>brands</p>
+                </Col>
+                <Col
+                    className={'d-flex justify-content-center flex-column align-items-center productSummary__products'}>
+                    <p>9<span>k</span></p>
+                    <p>products</p>
+                </Col>
+                <Col><img alt={palette} src={palette}/></Col>
+            </Row>
+        </Container>
     );
 };
 
