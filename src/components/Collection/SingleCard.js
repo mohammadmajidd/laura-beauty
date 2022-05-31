5a5260906fb2354db6b3c55615105688c288794c
mohammad <mohammadmajidi27@gmail.com>
2022-05-31 11:16:47 +0430

    edited Product Summary style-responsive

--- src/components/Collection/SingleCard.js
+++ src/components/Collection/SingleCard.js
@@ -1,14 +1,14 @@
 import './SingleCard.scss'
 import React from 'react';
 import {Card} from "react-bootstrap";
-import HoverImage from "react-hover-image/build";
+
 
 const SingleCard = (props) => {
     return (
         <div className='cardContainer'>
             <Card className={'border-0 fw-bold w-100 h-100 shadow cardContainer__singleCard'}>
                 <div className='cardContainer__singleCard__imageContainer'>
-                    <HoverImage className={'singleCard__imageContainer__imageCard'} hoverSrc={props.hoverImage} src={props.image}/>
+                    <Card.Img className={'singleCard__imageContainer__imageCard'} src={props.image}/>
                 </div>
                 <Card.Body>
                     <Card.Title className={'text-center cardContainer__singleCard__brandCard'}>{props.name}</Card.Title>
