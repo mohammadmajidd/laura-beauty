58a6119bed947a44a9ab625fabe78c009245d689
mohammad <mohammadmajidi27@gmail.com>
2022-05-31 10:30:11 +0430

    edited header style-responsive

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -5,10 +5,11 @@ import searchIcon from '../../../assets/navBar icons/search-interface-symbol.svg
 import userIcon from '../../../assets/navBar icons/ic user.svg'
 import shoppingIcon from '../../../assets/navBar icons/shopping-bag-of-normal-design-outline.svg'
 import './Header.scss'
+
 const Header = () => {
     return (
         <>
-            <Navbar>
+            <Navbar expand={"lg"}>
                 <Container className={'header'}>
                     <Navbar.Brand className={'header__logo'}>
                         <img alt={logo} src={logo}/>
@@ -17,18 +18,22 @@ const Header = () => {
                             <p>beauty</p>
                         </div>
                     </Navbar.Brand>
-
-                    <Nav className={'header__nav'}>
-                        <Nav.Link className={'me-4 ms-4'} href="#home">Brands</Nav.Link>
-                        <Nav.Link className={'me-4 ms-4'} href="#features">Products</Nav.Link>
-                        <Nav.Link className={'me-4 ms-4'} href="#pricing">Branches</Nav.Link>
-                        <Nav.Link className={'me-4 ms-4'} href="#price">About Us</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav className={'me-2 ms-2'}><img alt={searchIcon} src={searchIcon}/></Nav>
-                        <Nav className={'me-2 ms-2'}><img alt={userIcon} src={userIcon}/></Nav>
-                        <Nav className={'me-2 ms-2'}><img alt={shoppingIcon} src={shoppingIcon}/></Nav>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className={'header__nav'}>
+                            <Nav.Link className={'me-4 ms-4'} href="#home">Brands</Nav.Link>
+                            <Nav.Link className={'me-4 ms-4'} href="#features">Products</Nav.Link>
+                            <Nav.Link className={'me-4 ms-4'} href="#pricing">Branches</Nav.Link>
+                            <Nav.Link className={'me-4 ms-4'} href="#price">About Us</Nav.Link>
+                        </Nav>
+                        <Nav className={'header__navIcon'}>
+                            <Nav className={'me-2 ms-2 header__navIcon__iconContainer'}><img alt={searchIcon} src={searchIcon}/></Nav>
+                            <Nav className={'me-2 ms-2 header__navIcon__iconContainer'}><img alt={userIcon}
+                                                                                    src={userIcon}/></Nav>
+                            <Nav className={'me-2 ms-2 header__navIcon__iconContainer'}><img alt={shoppingIcon}
+                                                                                    src={shoppingIcon}/></Nav>
+                        </Nav >
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
